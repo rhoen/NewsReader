ef265385f5f51643da108fd21f3425f913b35842
rhoen+nathan <rhoen+nathan@appacademy.io>
2015-05-07 13:51:33 -0400

    add entries function to feed model

--- app/assets/javascripts/models/feed.js
+++ app/assets/javascripts/models/feed.js
@@ -1,6 +1,14 @@
 NewsReader.Models.Feed = Backbone.Model.extend({
   urlRoot: "/api/feeds",
   entries: function () {
-    //TODO
+    if (!this._entries) {
+      this._entries = new NewsReader.Collections.Entries([], {
+        feed: this
+      })
+    }
+
+    return this._entries;
   }
+
+
 })
