5c40c0b2155a51b0e1e02c2a7c8b20ddb3eb76e5
rhoen+nathan <rhoen+nathan@appacademy.io>
2015-05-07 17:25:31 -0400

    Add find_by_credentials and is_password? methods to ruby User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,31 @@
 class User < ActiveRecord::Base
-  validates :password_digest, :session_token, :username, presence: true
+  validates :password_digest, :username, presence: true
   validates :username, :session_token, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
   validate :ensure_session_token
 
+  attr_reader :password
+
   def self.secure_token
     SecureRandom::urlsafe_base64
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      return user
+    end
+
+    return nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   private
